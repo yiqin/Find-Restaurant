8f0196113d30952a32223446aab7c7b740fa9151
Adam Gerber <adam.gerber@yahoo.com>
2015-05-13 10:02:13 -0500

    third tab added, changing colors

--- app/src/main/java/samples/exoguru/materialtabs/MainActivity.java
+++ app/src/main/java/samples/exoguru/materialtabs/MainActivity.java
@@ -1,11 +1,17 @@
 package samples.exoguru.materialtabs;
 
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 /**
  * Created by Edwin on 15/02/2015.
@@ -18,8 +24,13 @@ public class MainActivity extends ActionBarActivity {
     ViewPager pager;
     ViewPagerAdapter adapter;
     SlidingTabLayout tabs;
-    CharSequence Titles[]={"Home","Events"};
-    int Numboftabs =2;
+    CharSequence Titles[]={"scan","cart", "pay"};
+    int Numboftabs =3;
+
+    //for collors
+    private Drawable oldBackground = null;
+    private int currentColor;
+    private SystemBarTintManager mTintManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,11 @@ public class MainActivity extends ActionBarActivity {
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        // create our manager instance after the content view is set
+        mTintManager = new SystemBarTintManager(this);
+        // enable status bar tint
+        mTintManager.setStatusBarTintEnabled(true);
 
 
         // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
@@ -48,6 +64,20 @@ public class MainActivity extends ActionBarActivity {
         tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
             public int getIndicatorColor(int position) {
+
+                switch(position){
+                    case 0:
+                        changeColor(getResources().getColor(R.color.green));
+                        break;
+                    case 1:
+                        changeColor(getResources().getColor(R.color.orange));
+                        break;
+                    case 2:
+                        changeColor(getResources().getColor(R.color.purple));
+                        break;
+                }
+
+
                 return getResources().getColor(R.color.tabsScrollColor);
             }
         });
@@ -59,7 +89,24 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    private void changeColor(int newColor) {
+        tabs.setBackgroundColor(newColor);
+        mTintManager.setTintColor(newColor);
+        // change ActionBar color just if an ActionBar is available
+        Drawable colorDrawable = new ColorDrawable(newColor);
+        Drawable bottomDrawable = new ColorDrawable(getResources().getColor(android.R.color.transparent));
+        LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
+        if (oldBackground == null) {
+            getSupportActionBar().setBackgroundDrawable(ld);
+        } else {
+            TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
+            getSupportActionBar().setBackgroundDrawable(td);
+            td.startTransition(200);
+        }
 
+        oldBackground = ld;
+        currentColor = newColor;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
