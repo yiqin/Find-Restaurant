bcf5e450b968825a514f355a3d3deb86b2e40513
Adam Gerber <adam.gerber@yahoo.com>
2015-05-14 00:45:01 -0500

    working with search view fragment

--- app/src/main/java/gerber/uchicago/edu/Tab1.java
+++ app/src/main/java/gerber/uchicago/edu/Tab1.java
@@ -16,7 +16,7 @@ public class Tab1 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.tab_1,container,false);
+        View v =inflater.inflate(R.layout.activity_edit_resto,container,false);
         return v;
     }
 }
