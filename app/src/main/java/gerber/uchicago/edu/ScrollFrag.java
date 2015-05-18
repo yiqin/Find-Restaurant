ad359023d9c13916c235ae212a77ece1d977d64e
Adam Gerber <adam.gerber@yahoo.com>
2015-05-17 23:16:21 -0500

    scrollview crashing becaue of one host only

---
+++ app/src/main/java/gerber/uchicago/edu/ScrollFrag.java
@@ -0,0 +1,39 @@
+package gerber.uchicago.edu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Edwin on 15/02/2015.
+ */
+public class ScrollFrag extends Fragment {
+
+
+    public static final String LAYOUT_KEY = "LAYOUT";
+
+    public static ScrollFrag getInstance(Bundle bundle){
+
+        ScrollFrag scrollFrag = new ScrollFrag();
+        scrollFrag.setArguments(bundle);
+        return scrollFrag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        int nLayout = 0;
+        try {
+            nLayout = getArguments().getInt(LAYOUT_KEY);
+        } catch (Exception e) {
+           // e.printStackTrace();
+            nLayout = R.layout.frag_scroll_layout_new;
+        }
+
+        View v = inflater.inflate(nLayout,container,false);
+        return v;
+    }
+}
--- app/src/main/java/gerber/uchicago/edu/Tab1.java
+++ app/src/main/java/gerber/uchicago/edu/Tab1.java
@@ -3,94 +3,144 @@ package gerber.uchicago.edu;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.Transformation;
-import android.widget.LinearLayout;
-import android.widget.ToggleButton;
-
-import gerber.uchicago.edu.R;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * Created by Edwin on 15/02/2015.
  */
 public class Tab1 extends Fragment {
 
-    ToggleButton mToggleButton;
-    LinearLayout mLinearLayoutDetails;
+//    ToggleButton mToggleButton;
+//    LinearLayout mLinearLayoutDetails;
+
+
+    private RadioGroup mRadioGroup;
+    private FragmentTransaction mFragmentTransaction;
+    private FragmentManager mFragmentManager;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.activity_edit_resto,container,false);
-        mToggleButton = (ToggleButton) v.findViewById(R.id.toggle_btn);
-        mLinearLayoutDetails = (LinearLayout) v.findViewById(R.id.details_layout);
 
+//        btnTabEdit = (RadioButton) v.findViewById(R.id.btn_tab_edit);
+//        btnTabNew = (RadioButton) v.findViewById(R.id.btn_tab_new);
 
-        mToggleButton.setOnClickListener(new View.OnClickListener() {
+        mRadioGroup = (RadioGroup) v.findViewById(R.id.rdogrp);
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.btn_tab_edit){
 
-                Animation anim;
-                if (mToggleButton.isChecked()) {
+                    mFragmentManager =
+                            getActivity().getSupportFragmentManager();
+                    mFragmentTransaction = mFragmentManager.beginTransaction();
 
-                   anim = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_down);
-                    anim.setAnimationListener(new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-                            mLinearLayoutDetails.setVisibility(View.VISIBLE);
-                        }
+                    Bundle bundle  =new Bundle();
+                    bundle.putInt(ScrollFrag.LAYOUT_KEY, R.layout.frag_scroll_layout_edit);
+                    ScrollFrag frag = ScrollFrag.getInstance(bundle);
 
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-
-                        }
-                    });
-                    if(anim != null){
-                        anim.reset();
-                        mLinearLayoutDetails.clearAnimation();
-                        mLinearLayoutDetails.startAnimation(anim);
-
-                    }
-
-                } else {
-
-                    anim = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_up);
-                    anim.setAnimationListener(new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-                            mLinearLayoutDetails.setVisibility(View.GONE);
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-
-                        }
-                    });
-                    if(anim != null){
-                        anim.reset();
-                        mLinearLayoutDetails.clearAnimation();
-                        mLinearLayoutDetails.startAnimation(anim);
-
-                    }
+                    mFragmentTransaction.replace(R.id.data_root_view_group, frag);
 
 
 
                 }
+                //new is clicked
+                else {
+
+                    mFragmentManager =
+                            getActivity().getSupportFragmentManager();
+                    mFragmentTransaction = mFragmentManager.beginTransaction();
+
+                    Bundle bundle  =new Bundle();
+                    bundle.putInt(ScrollFrag.LAYOUT_KEY, R.layout.frag_scroll_layout_new);
+                    ScrollFrag frag = ScrollFrag.getInstance(bundle);
+
+                    mFragmentTransaction.replace(R.id.data_root_view_group, frag);
+
+
+                }
+                mFragmentTransaction.commit();
+
+
+
             }
         });
+
+
+//        mToggleButton = (ToggleButton) v.findViewById(R.id.toggle_btn);
+//        mLinearLayoutDetails = (LinearLayout) v.findViewById(R.id.details_layout);
+//
+//
+//        mToggleButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Animation anim;
+//                if (mToggleButton.isChecked()) {
+//
+//                   anim = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_down);
+//                    anim.setAnimationListener(new Animation.AnimationListener() {
+//                        @Override
+//                        public void onAnimationStart(Animation animation) {
+//                            mLinearLayoutDetails.setVisibility(View.VISIBLE);
+//                        }
+//
+//                        @Override
+//                        public void onAnimationEnd(Animation animation) {
+//
+//                        }
+//
+//                        @Override
+//                        public void onAnimationRepeat(Animation animation) {
+//
+//                        }
+//                    });
+//                    if(anim != null){
+//                        anim.reset();
+//                        mLinearLayoutDetails.clearAnimation();
+//                        mLinearLayoutDetails.startAnimation(anim);
+//
+//                    }
+//
+//                } else {
+//
+//                    anim = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_up);
+//                    anim.setAnimationListener(new Animation.AnimationListener() {
+//                        @Override
+//                        public void onAnimationStart(Animation animation) {
+//
+//                        }
+//
+//                        @Override
+//                        public void onAnimationEnd(Animation animation) {
+//                            mLinearLayoutDetails.setVisibility(View.GONE);
+//                        }
+//
+//                        @Override
+//                        public void onAnimationRepeat(Animation animation) {
+//
+//                        }
+//                    });
+//                    if(anim != null){
+//                        anim.reset();
+//                        mLinearLayoutDetails.clearAnimation();
+//                        mLinearLayoutDetails.startAnimation(anim);
+//
+//                    }
+//
+//
+//
+//                }
+//            }
+//        });
         return v;
     }
 
